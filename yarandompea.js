0a3409bf8baf6e0853589b12f069f851e195a0e4
Evgeniy Pakhomov <nightmare23@yandex.ru>
2024-06-30 02:16:46 +0700

    Update yarandompea.js

--- yarandompea.js
+++ yarandompea.js
@@ -12,7 +12,6 @@ function randomDay1() {
     randomDay = Math.floor(Math.random() * (maxDay - minDay) + minDay);
     return randomDay;
 }
-//let randomDay = Math.floor(Math.random() * (maxDay - minDay) + minDay);
 
 function randomMonth1() {
     randomMonth = Math.floor(Math.random() * (maxMonth - minMonth) + minMonth);
@@ -27,45 +26,16 @@ function randomMonth1() {
         return randomMonth;
     }
 }
-// return mm;
-//let randomMonth = Math.floor(Math.random() * (maxMonth - minMonth) + minMonth);
 
 function randomYear1() {
     randomYear = Math.floor(Math.random() * (maxYear - minYear) + minYear);
     console.log(randomMonth);
     if ((((randomYear % 4 !== 0 && randomYear % 100 === 0) || (randomYear % 4 !== 0 && randomYear % 100 !== 0 && randomYear % 400 !== 0)) && randomDay > 28)) {
-        //randomYear = 2021;
-        //randomYear1();
-        return randomYear1();
+       return randomYear1();
     }
-
-    /*if (((randomYear % 4 === 0 && randomYear % 100 !== 0) || (randomYear % 4 === 0 && randomYear % 100 === 0 && randomYear % 400 === 0)) && (randomDay > 29)) {
-        return randomYear1();
-    }*/
     else {
         return randomYear;
     }
 
 }
-
 module.exports = {randomDay1,randomMonth1,randomYear1};
-/*function verifyDate(randomDay, randomMonth, randomYear) {
-    if (!(randomYear % 4 === 0 && randomYear % 100 !== 0) || !(randomYear % 4 === 0 && randomYear % 100 === 0 && randomYear % 400 === 0) && (randomDay === 29)) {
-        return randomDay1(1, 30);
-    }
-    if (randomDay === 31 && (randomMonth === 2 || randomMonth === 4 || randomMonth === 6 || randomMonth === 9 || randomMonth === 11)) {
-        return randomDay1(1, 30);
-    }
-    else {
-        return verifyDate;
-    }
-}*/
-//let randomYear = Math.floor(Math.random() * (maxYear - minYear) + minYear);
-
-/*if (mm === 5) {
-    randomMonth1(1,randomMonth1());
-}*/
-//console.log(randomDay, randomMonth, randomYear);
-//if (randomDay === 30) { console.log("!"); }
-console.log(randomDay1(), randomMonth1(), randomYear1());
-//console.log(verifyDate(randomDay1(), randomMonth1(), randomYear1()));
